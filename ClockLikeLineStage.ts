89cf02b1b38d8019e3713a0fedfa9328e229a942
Anwesh43 <anweshthecool0@gmail.com>
2018-10-09 10:48:25 +0530

    adding CLLNode class

--- ClockLikeLineStage.ts
+++ ClockLikeLineStage.ts
@@ -1,5 +1,10 @@
 const w : number = window.innerWidth, h : number = window.innerHeight
 const nodes : number = 5
+const getAngles = (h, m) => {
+    const ha = 360/12
+    const ma = 360/60
+    return [Math.floor(h * ha) + (ma/60) * ha, ma *m]
+}
 class ClockLikeLineStage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
@@ -71,5 +76,65 @@ class Animator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class CLLNode {
+    next : CLLNode
+    prev : CLLNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new CLLNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap = w / (nodes + 1)
+        const weightFactors = [30, 60]
+        const lengthFactors = [4, 2]
+        context.strokeStyle = '#BDBDBD'
+        const angles = getAngles(4, 30)
+        context.save()
+        context.translate(gap * this.i + gap, h/2)
+        for (var j = 0; j < 2; j++) {
+            const sc = Math.min(0.5, Math.max(this.state.scale - 0.5 * j, 0)) * 2
+            context.lineCap = 'round'
+            context.lineWidth = Math.min(w, h) / weightFactors[j]
+            context.save()
+            context.rotate(angles[j] * Math.PI/180 * sc)
+            context.beginPath()
+            context.moveTo(0, 0)
+            context.lineTo(0, -gap/lengthFactors[j])
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : CLLNode {
+        var curr : CLLNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
     }
 }
